fbc941b8d7bbd3dd500033e09683aa3aef26f5f0
Mike Davis <mike.davis@psyvad.com>
2023-09-11 10:42:01 -0600

    Created program that takes user input to inform them of grade earned in class.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What grade percentage did you receive? ");
+        string userGrade = Console.ReadLine();
+        int userGradeAsInt = int.Parse(userGrade);
+
+        string letterGrade = "";
+
+        if (userGradeAsInt >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (userGradeAsInt >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (userGradeAsInt >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (userGradeAsInt >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.Write($"You received a(n) {letterGrade}. ");
+
+        if (userGradeAsInt >= 70) {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else {
+            Console.WriteLine("Try again!");
+        }
     }
 }
