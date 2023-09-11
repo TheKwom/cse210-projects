c05a5b98c0ceb4dd4cd72133c2dec7598fddf125
Mike Davis <mike.davis@psyvad.com>
2023-09-11 12:06:03 -0600

    Created a random number generator that prompts user for guesses un til user is correct

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rnd = new Random();
+        int magicNum = rnd.Next(1, 100);
+
+        bool correct = false;
+
+        while (correct == false) {
+            Console.WriteLine("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            int userGuessAsInt = int.Parse(userGuess);
+            if (userGuessAsInt < magicNum) {
+                Console.WriteLine("Higher");
+            } else if (userGuessAsInt > magicNum) {
+                Console.WriteLine("Lower");
+            } else {
+                correct = true;
+                }
+        }
+
+        Console.WriteLine("You guesses it!");
+
     }
 }
