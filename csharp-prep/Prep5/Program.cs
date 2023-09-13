7d3d81094d86b55273a4578048080af3c1f2fc20
Mike Davis <mike.davis@psyvad.com>
2023-09-13 16:02:55 -0600

    Completed Prep #5. Using functions and methods.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+
+        // DisplayWelcome();
+        // DisplayResult(PromptUserName(), SquareNumber(PromptUserNumber()));
+
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squareNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, squareNumber);
+
+        static void DisplayWelcome() {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName() {
+            Console.WriteLine("Please enter your name: ");
+            string username = Console.ReadLine();
+            return username;
+        }
+
+        static int PromptUserNumber() {
+            Console.WriteLine("Please enter your favorite number: ");
+            string userNumber = Console.ReadLine();
+            int userNumberAsInt = int.Parse(userNumber);
+            return userNumberAsInt;
+        }
+
+        static int SquareNumber(int userNumber) {
+            int squaredUserNumber = userNumber * userNumber;
+            return squaredUserNumber;
+        }
+
+        static void DisplayResult(string userName, int squaredUserNumber) {
+            Console.WriteLine($"{userName}, the square of your number is {squaredUserNumber}");
+        }
     }
 }
